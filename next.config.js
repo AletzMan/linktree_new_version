380f81fffbb2b5f8189f2647db964aa0c55f835c
= <=>
2023-06-16 15:14:08 -0600

    Init commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    skipMiddlewareUrlNormalize: true,
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+    },
+}
 
 module.exports = nextConfig
