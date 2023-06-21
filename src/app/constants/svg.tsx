13ff8b4b76303a4b230b5f9fde775ba29adbb8a3
= <=>
2023-06-21 14:39:00 -0600

    Added picker color settings

--- src/app/constants/svg.tsx
+++ src/app/constants/svg.tsx
@@ -359,4 +359,13 @@ export function AddIcon({ className }: { className: string }) {
     )
 }
 
+export function ColorIcon({ className }: { className: string }) {
+    return (
+        <svg className={className} width="28" height="28" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M7.75 8.99A5.766 5.766 0 0 0 6 13.13C6 16.37 8.69 19 12 19V4.81L7.75 8.99z" opacity=".3" />
+            <path fill="currentColor" d="M17.65 7.56L12 2L6.35 7.56C4.9 8.99 4 10.96 4 13.13C4 17.48 7.58 21 12 21s8-3.52 8-7.87c0-2.17-.9-4.14-2.35-5.57zM6 13.13c0-1.56.62-3.03 1.75-4.14L12 4.81V19c-3.31 0-6-2.63-6-5.87z" />
+        </svg>
+    )
+}
+
 
