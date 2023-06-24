36706593a2e875fa5d64ff06d9ec4e060b0c43bb
= <=>
2023-06-24 03:05:02 -0600

    Changed: URL redirect for deploy

--- src/app/components/auth-form.tsx
+++ src/app/components/auth-form.tsx
@@ -42,7 +42,7 @@ export default function AuthForm() {
             }}
             showLinks={false}
             providers={['google', 'github']}
-            redirectTo="http://localhost:3000/auth/callback"
+            redirectTo="http://linktree-new-version.vercel.app/auth/callback"
         />
     )
 }
